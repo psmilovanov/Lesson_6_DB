11dd87da72bf587cc69730d13ec525ed20d51357
psmilovanov <psmilovanov@gmail.com>
2020-06-14 22:54:41 +0300

    Create file for Lesson_6_DB

---
+++ Lesson_6_DB_hw.sql
@@ -0,0 +1,91 @@
+use vk;
+
+/*Задача 2
+ * Созданный запрос работает только для исходящих сообщений от пользователя с user_id = 1.
+ * Также необходимо изменить запрос, чтобы он использовал коррелированные переменные. Я так и не разобрался как это делать правильно.
+ * 
+ */
+
+select to_user_id, count(to_user_id) as numbers
+	from messages where from_user_id=1
+	and (to_user_id in
+	(
+		(select target_user_id from friend_requests where (initiator_user_id = 1) and (status = 'approved'))
+		union 
+		(select initiator_user_id from friend_requests where (target_user_id = 1) and (status = 'approved'))
+	)
+	)
+group by to_user_id order by numbers desc limit 1;
+
+/* Чтобы считать и отправленные и входящие сообщения я пробовал использовать функцию UNION 
+ * 
+ * 
+ * Таким запросом я составил таблицу из входящих и исходящих для пользоватлея c user_id = 1 (необязательно друзья), 
+ * но как работать с этой таблицей не понятно. 
+ * select to_user_id as user_id, count(to_user_id) as numbers from messages where from_user_id=1 group by to_user_id
+union all
+select from_user_id, count(from_user_id) from messages where to_user_id=1 group by from_user_id; 
+
+ Даже такой запрос 
+ 
+  select * from 
+(select to_user_id as user_id, count(to_user_id) as numbers from messages where from_user_id=1 group by to_user_id
+union all
+select from_user_id, count(from_user_id) from messages where to_user_id=1 group by from_user_id);
+приводит к ошибке 
+
+ QL Error [1248] [42000]: Every derived table must have its own alias 
+ * */
+
+
+
+/*задача 3. 
+ * Первоначально я создал такой запрос
+ * 
+ * select * from likes where userid in (select userid from profiles order by birthday desc limit 10);
+ * Получил такую ошибку SQL Error [1235] [42000]: This version of MySQL doesn't yet support 'LIMIT & IN/ALL/ANY/SOME subquery'
+ * 
+ * Наставник посоветовал изменить запрос на 
+ * COUNT(*)
+FROM likes
+WHERE userid IN ( SELECT userid FROM profiles WHERE TIMESTAMPDIFF (YEAR, NOW(), birthday) < 10
+);
+ * Этот запрос не соответвует условиям задачи. Да и считает лайки всех пользователей. Я исправил немного этот запрос,
+ * поменяв местами NOW() и birthday. 
+ * Он считает лайки всех пользователей младше 10 лет. 
+ */
+select count(*) from likes where user_id in (select user_id from profiles where TIMESTAMPDIFF (YEAR, birthday, NOW()) < 10);
+
+/*Задача 4. Здесь вроде все получилось.*/
+
+select  
+case (select gender from profiles where user_id = id)
+	when 'f' then 'Женщины'
+	when 'm' then 'Мужчины'
+end as gender_l,
+count(*) as Number_of_likes from likes group by gender_l order by Number_of_likes desc limit 1;
+
+/*Задача 5. С помощью union не получается выполнить запрос. Пришлось создать дополнительную таблицу и с ней работать*/
+
+
+drop table if exists table_of_activite;
+create table table_of_activite (
+	id SERIAL primary key,
+	user_id BIGINT unsigned not null,
+	numbers_of_activity BIGINT unsigned not null,
+	foreign key (user_id) references users(id)
+);
+
+insert into `table_of_activite` (`user_id`,`numbers_of_activity`)  select user_id, count(user_id) from media group by user_id;
+insert into `table_of_activite` (`user_id`,`numbers_of_activity`)  select user_id, count(user_id) from likes group by user_id;
+insert into `table_of_activite` (`user_id`,`numbers_of_activity`)  select initiator_user_id, count(initiator_user_id) from friend_requests group by initiator_user_id;
+/*Здесь необходимо добавить также по остальным активностям, например, из таблицы messages, и фотоальбом*/
+select user_id, sum(numbers_of_activity) as total_number from table_of_activite group by user_id order by total_number limit 10;
+
+/*Запрос через UNION выводит ошибку*/
+select user_id, sum(numbers_of_act) as tot_number from 
+(select user_id, count(user_id) as numbers_of_act from media group by user_id
+union all
+select user_id, count(user_id) from likes group by user_id) 
+
+group by user_id order by tot_number limit 10;
